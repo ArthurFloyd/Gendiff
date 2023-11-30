9e03c8e8e81d5e1887200cbbaee95f3362f8c45e
arthur voryshev <what_to_write@mail.ru>
2023-12-01 00:17:53 +0300

    update stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,19 +1,20 @@
 import _ from 'lodash';
 
+const stringify = (data, depth) => {
+  if (!_.isObject(data)) {
+    return `${data}`;
+  }
+  const repeat = '    '.repeat(depth + 1);
+  const line = Object.entries(data).map(([key, val]) => {
+    const preparedValue = stringify(val, depth + 1);
+    return `${repeat}${key}: ${preparedValue}`;
+  });
+  const outRepeat = '    '.repeat(depth);
+  const rawResult = ['{', ...line, `${outRepeat}}`].join('\n');
+  return rawResult;
+};
+
 const stylish = (diffTree, replacer = '    ', spacesCount = 1) => {
-  const iter = (data, depth) => {
-    if (!_.isObject(data)) {
-      return `${data}`;
-    }
-    const repeat = replacer.repeat(depth + 1);
-    const line = Object.entries(data).map(([key, val]) => {
-      const preparedValue = iter(val, depth + 1);
-      return `${repeat}${key}: ${preparedValue}`;
-    });
-    const outRepeat = replacer.repeat(depth);
-    const rawResult = ['{', ...line, `${outRepeat}}`].join('\n');
-    return rawResult;
-  };
   const lines = [];
   const indentation = replacer.repeat(spacesCount);
   diffTree.forEach(({ key, value, status }) => {
@@ -22,17 +23,17 @@ const stylish = (diffTree, replacer = '    ', spacesCount = 1) => {
         lines.push(`${indentation}${key}: ${stylish(value, replacer, spacesCount + 1)}`);
         break;
       case 'added':
-        lines.push(`${indentation.slice(2)}+ ${key}: ${iter(value, spacesCount)}`);
+        lines.push(`${indentation.slice(2)}+ ${key}: ${stringify(value, spacesCount)}`);
         break;
       case 'deleted':
-        lines.push(`${indentation.slice(2)}- ${key}: ${iter(value, spacesCount)}`);
+        lines.push(`${indentation.slice(2)}- ${key}: ${stringify(value, spacesCount)}`);
         break;
       case 'changed':
-        lines.push(`${indentation.slice(2)}- ${key}: ${iter(value[0], spacesCount)}`);
-        lines.push(`${indentation.slice(2)}+ ${key}: ${iter(value[1], spacesCount)}`);
+        lines.push(`${indentation.slice(2)}- ${key}: ${stringify(value[0], spacesCount)}`);
+        lines.push(`${indentation.slice(2)}+ ${key}: ${stringify(value[1], spacesCount)}`);
         break;
       case 'unchanged':
-        lines.push(`${indentation}${key}: ${iter(value, 1)}`);
+        lines.push(`${indentation}${key}: ${stringify(value, 1)}`);
         break;
       default:
         throw new Error(`Unknown status: '${status}'!`);
