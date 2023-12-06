5ca42f0f9927f106ad8955479747606aa2a6ec31
arthur voryshev <what_to_write@mail.ru>
2023-12-06 15:53:46 +0300

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,20 +1,20 @@
-// import { fileURLToPath } from 'url';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 
-// const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const extention = ['json', 'yml'];
 
 describe('gendiff', () => {
-  const stylishTestResult = fs.readFileSync(path.resolve('./__fixtures__/stylishTestResult.txt'), 'utf8');
-  const plainTestResult = fs.readFileSync(path.resolve('./__fixtures__/plainTestResult.txt'), 'utf8');
-  const jsonTestResult = fs.readFileSync(path.resolve('./__fixtures__/jsonTestResult.txt'), 'utf8');
+  const stylishTestResult = fs.readFileSync(path.resolve(__dirname, './__fixtures__/stylishTestResult.txt'), 'utf8');
+  const plainTestResult = fs.readFileSync(path.resolve(__dirname, './__fixtures__/plainTestResult.txt'), 'utf8');
+  const jsonTestResult = fs.readFileSync(path.resolve(__dirname, './__fixtures__/jsonTestResult.txt'), 'utf8');
   describe.each(extention)('compare two %s files', (ext) => {
-    const before = path.resolve(`./__fixtures__/before.${ext}`);
-    const after = path.resolve(`./__fixtures__/after.${ext}`);
+    const before = path.resolve(__dirname, `./__fixtures__/before.${ext}`);
+    const after = path.resolve(__dirname, `./__fixtures__/after.${ext}`);
     const expected = genDiff(before, after);
-    console.log(`./__fixtures__/before.${ext}`);
+    console.log(__dirname, `./__fixtures__/before.${ext}`);
 
     test('stylish', () => {
       expect(expected).toBe(stylishTestResult);
